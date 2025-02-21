a850dd273a0719f4069809f9258f4b9471f8576e
Dhairya2505 <dhairyasingla250504@gmail.com>
2025-02-21 22:45:37 +0530

    server url change

--- app/constants.ts
+++ app/constants.ts
@@ -1,2 +1,2 @@
-export const BACKEND_CHAT_SERVER = "wss://baatcheetbackend-production.up.railway.app/"
-export const BACKEND_URI = "https://baatcheetbackend-production.up.railway.app/"
+export const BACKEND_CHAT_SERVER = "wss://baatcheetbackend-production.up.railway.app"
+export const BACKEND_URI = "https://baatcheetbackend-production.up.railway.app"
